c84ae543fbd6ec32ff15a4cb07e89c986863fa37
Wanna09 <minwannahlan@gmail.com>
2025-05-14 12:16:35 +0700

    First Commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,21 @@
+package com.db_test;
+
+
+/**
+    This is testing Main Class.
+    Created by: ......
+    Created Date: ......
+*/
+
+public class Main {
+
+    /**
+
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+
+
+    }
+}
