892919aba0cc2bb7c9faa916fb3f9759e65f21a1
Wanna09 <minwannahlan@gmail.com>
2025-05-19 16:42:18 +0700

    Database connection.

--- src/main/java/com/db_test/Main.java
+++ src/main/java/com/db_test/Main.java
@@ -1,21 +1,44 @@
 package com.db_test;
 
 
-/**
-    This is testing Main Class.
-    Created by: ......
-    Created Date: ......
-*/
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 
 public class Main {
 
-    /**
+    private String ip = "localhost";
+    private String port = "3306";
+    private String dbname = "test_db";
+    private String username = "root";
+    private String password = "";
 
-     */
+    protected Connection get_Db_connection(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://" + ip + ":" + port + "/" + dbname,
+                    username, password);
+            System.out.println("Successful Connection.");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
+
     public static void main(String[] args) {
+        Main m = new Main();
+        Connection con = m.get_Db_connection();
 
-        System.out.println("Hello and welcome!");
-
+        try{
+            if(con != null){
+                con.close();
+                System.out.println("Connection closed.");
+            }
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
 
     }
 }
